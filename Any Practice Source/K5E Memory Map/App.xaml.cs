28bb3645e36072767a24df7f84875a5d01c4ed35
Goal3-14 <goalcreeper314@gmail.com>
2025-01-01 21:35:51 +0000

    Start v1

---
+++ Any Practice Source/K5E Memory Map/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using System.Threading.Tasks;
+using K5E_Memory_Map.UIModule;
+using System.Diagnostics;
+
+namespace K5E_Memory_Map
+{
+    public partial class App : Application
+    {
+        public MainLoop? MainLoop { get; private set; } // Property to access MainLoop
+        private MainWindow? _mainWindow;
+
+        public InfoLoop? InfoLoop { get; private set; } // Property to access MainLoop
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            _mainWindow = new MainWindow();
+            _mainWindow.Show(); // Show the main window
+            MainLoop = new MainLoop(_mainWindow);
+            Task.Run(() => MainLoop.Run(null));
+
+            //_MemDisplay = new MemDisplay();
+            //_MemDisplay.Show(); // Show the main window
+            InfoLoop = new InfoLoop(_mainWindow);
+            Task.Run(() => InfoLoop.Run());
+
+            Debug.WriteLine("affafg");
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            MainLoop?.Stop(); // Stop the main loop on exit
+            InfoLoop?.Stop();
+            base.OnExit(e);
+        }
+    }
+}
---
+++ Any Practice Source/K5E Memory Map/MainLoop.cs
@@ -0,0 +1,380 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+using System.Threading.Tasks.Dataflow;
+using System.Windows;
+using System.Windows.Controls;
+using System.IO.MemoryMappedFiles;
+using System.IO;
+using System.Security.Cryptography;
+using System.Windows.Markup;
+using System.Diagnostics.Eventing.Reader;
+using K5E_Memory_Map.UIModule;
+using System.Windows.Threading;
+namespace K5E_Memory_Map
+{
+    
+
+
+
+    public class MainLoop
+    {
+        private readonly MainWindow _MainWindow;
+        private CancellationTokenSource _cancellationTokenSource;
+
+        public string MemText;
+        public int Frame;
+
+        private int StartFrame;
+        private string StartMem;
+
+        private string StartMemS;
+        private string StartMemE;
+
+        public TreeNode CurrentNode;
+        private TreeNode BufferNode;
+
+        public bool Paused = false;
+        public bool Loading = false;
+        private int Cycle = 0;
+
+        public Dictionary<string, TreeNode> NodeHash;
+        public Dictionary<string, TreeNode>? LoadNodeHash = null;
+
+
+        string FmapName = "FrameMemory";
+        string mapName = "MySharedMemory";
+
+
+        public MainLoop(MainWindow mainWindow)
+        {
+
+            _MainWindow = mainWindow;
+            _MainWindow.SSaveMenu._mainLoop = this;
+            _cancellationTokenSource = new CancellationTokenSource();
+            
+
+        }
+
+
+
+
+
+
+        //Get Data From Memory Viewer
+
+        private (string,int) GetValues()
+        {
+            while (true)
+            {
+                string? M = GetMem();
+                int? F = GetFrame();
+                if ((M != null) && (F != null))
+                {
+                    return (M, (int)F);
+                }
+                Thread.Sleep(1);
+            }
+
+        }
+
+        private string? GetMem() 
+        {
+            
+            try
+            {
+                using (var mmf = MemoryMappedFile.OpenExisting(mapName))
+                {
+                    using (var accessor = mmf.CreateViewAccessor(0, 1024, MemoryMappedFileAccess.Read)) // Adjust size as necessary
+                    {
+                        byte[] buffer = new byte[1024]; // Buffer size must match what you wrote
+                        accessor.ReadArray(0, buffer, 0, buffer.Length);
+                        string memText = Encoding.UTF8.GetString(buffer).Trim('\0'); // Convert to string and trim null characters
+                        if (memText == "")
+                        {
+                            return null;
+                        }
+                        return memText;
+                    }
+                }
+            }
+            catch (IOException ex)
+            {   
+                //Console.WriteLine("Could not open memory-mapped file for MemText: " + ex.Message);
+                return null;
+            }
+            
+            
+        }
+
+        private int? GetFrame()
+        {
+            
+            try
+            {
+                using (var mmfFrame = MemoryMappedFile.OpenExisting(FmapName))
+                {
+                    using (var accessor = mmfFrame.CreateViewAccessor(0, 1024, MemoryMappedFileAccess.Read)) // Adjust size as necessary
+                    {
+                        byte[] buffer = new byte[1024]; // Buffer size must match what you wrote
+                        accessor.ReadArray(0, buffer, 0, buffer.Length);
+                        string frame = Encoding.UTF8.GetString(buffer).Trim('\0'); // Convert to string and trim null characters
+                        try
+                        {
+                            return Convert.ToInt32(frame);
+                        }
+                        catch (FormatException)
+                        {
+                            return null;
+                        }
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+                //Console.WriteLine("Could not open memory-mapped file for frame: " + ex.Message);
+                return null;
+            }
+        }
+
+
+
+
+        public (string, string) SubText(string Mem)
+        {
+            string Start = StartMem.Substring(0, 4);
+            string End = StartMem.Substring(28);
+
+            return (Start, End);
+        }
+
+
+
+
+        public (int,string,TreeNode) Reset( Dictionary<string,TreeNode> NodeHash)
+        {
+            
+            string M = "";
+            int F = 0;
+            TreeNode CNode = null;
+
+            while (true)
+            {
+
+
+
+                (M, F) = GetValues();
+
+                if (NodeHash.TryGetValue(M, out TreeNode foundObject))
+                {
+                    CNode = foundObject;
+                    break;
+                }
+
+                Thread.Sleep(5);
+            }
+
+            return (F, M, CNode);
+
+
+        }
+
+
+
+
+
+
+
+        public void Run(Dictionary<string, TreeNode>? LoadNodeHash)
+        {
+            Debug.WriteLine("Thread Start");
+            
+            NodeHash = new Dictionary<string, TreeNode>();
+            //TreeNode root = new TreeNode("95C99029EE2F5684DADC658F79FF51BA", NodeHash, null);
+
+            MemoryMappedFile mmfB;
+
+            try
+            {
+                mmfB = MemoryMappedFile.OpenExisting(mapName);
+            }
+            catch (IOException)
+            {
+                mmfB = MemoryMappedFile.CreateNew(mapName, 32);
+            }
+
+            try
+            {
+                mmfB = MemoryMappedFile.OpenExisting(FmapName);
+            }
+            catch (IOException)
+            {
+                mmfB = MemoryMappedFile.CreateNew(FmapName, 8);
+            }
+
+
+            _MainWindow.Process = "2";
+            (StartFrame, StartMem, CurrentNode) = Reset(NodeHash);
+            (StartMemS, StartMemE) = SubText(StartMem);
+            Debug.WriteLine("BEGIN");
+            Debug.WriteLine(StartFrame);
+
+
+            // Get the cancellation token
+            var token = _cancellationTokenSource.Token;
+            _MainWindow.Process = "1";
+
+
+
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            int iterations = 0;
+            double elapsedSeconds = 0;
+
+            
+
+
+            while (!token.IsCancellationRequested)
+            {
+               
+                if ((_MainWindow.Paused == true) || (Loading == true))
+                {
+                    
+                }
+                else
+                {
+                    
+
+                    (MemText, Frame) = GetValues();
+                    _MainWindow.Hash = MemText;
+                    //_MainWindow.Frame = Frame;
+
+
+
+
+
+
+
+
+
+
+                    if ((MemText.Substring(0, 4) != StartMemS) && (MemText.Substring(28) != StartMemE)) //Check memory is different and check havent read Address mid read and gotten half new value half old
+                    {
+
+                        if (NodeHash.TryGetValue(MemText, out TreeNode foundObjectt))
+                        {
+                            //Node exists
+                            CurrentNode = foundObjectt;
+                            //CurrentNode.AddChild(BufferNode);
+                            //BufferNode.AddParent(CurrentNode);
+                            //CurrentNode = BufferNode;
+
+                        }
+                        else //Node doesnt exist
+                        {
+                            //Debug.WriteLine("Node Added");
+
+                            Thread.Sleep(1);
+                            //Sometimes when loading a state the frame count changes and the memory doesnt update until a read later
+                            //so if state loaded it waits briefly before getting memory
+                            //to ensure it uses memory after state not before as it will cause loops in the tree
+
+                            (MemText, Frame) = GetValues();
+                            _MainWindow.Frame = Frame;
+
+
+                            if (NodeHash.TryGetValue(MemText, out TreeNode foundObject))
+                            {
+                                //Node exists
+                                CurrentNode = foundObject;
+                                StartFrame = (int)Frame;
+                                StartMem = MemText;
+                                (StartMemS, StartMemE) = SubText(StartMem);
+
+                            }
+                            else //Keep trying to find Memory in tree 
+                            {
+                                _MainWindow.Process = "2";
+                                (StartFrame, StartMem, CurrentNode) = Reset(NodeHash);
+                                (StartMemS, StartMemE) = SubText(StartMem);
+                                //_MainWindow.Process = "1";
+                            }
+                        }
+
+
+
+
+
+                        //CurrentNode = BufferNode;
+                        StartFrame = (int)Frame;
+                        StartMem = MemText;
+                        (StartMemS, StartMemE) = SubText(StartMem);
+                    }
+                    else
+                    {
+                        StartFrame = (int)Frame;
+                    }
+
+
+
+
+                    
+                    
+
+                    Cycle++;
+                }
+
+
+
+                
+                if (Cycle == 100)
+                    {
+                    _MainWindow.Dispatcher.Invoke(() =>
+                    {
+                        _MainWindow.UpdateLoop(CurrentNode,NodeHash);
+                        //Debug.WriteLine(NodeHash.Count);
+                    });
+
+                    Cycle = 0;
+                    }
+
+
+
+
+
+                Thread.Sleep(1);
+
+
+
+
+                iterations++;
+
+                if (stopwatch.Elapsed.TotalSeconds >= 1)
+                {
+                    elapsedSeconds = stopwatch.Elapsed.TotalSeconds;
+                    Debug.WriteLine($"Iterations per second: {iterations / elapsedSeconds:F2}");
+                    iterations = 0;
+                    stopwatch.Restart();
+                }
+            }
+        }
+
+        
+        
+        
+
+
+
+
+
+        
+        
+        public void Stop()
+        {
+            _cancellationTokenSource.Cancel();
+        }
+    }
+}
